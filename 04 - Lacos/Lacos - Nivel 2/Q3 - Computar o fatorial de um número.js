8f65f3fb6d7a66d5aa1d75ce4c6f6e5c9a901ca0
Geneton <gasn1@discente.ifpe.edu.br>
2022-10-29 16:16:22 -0300

    deletando atividades incompletas de nivel dois

--- 04 - Lacos/Lacos - Nivel 2/Q3 - Computar o fatorial de um número.js
+++
@@ -1,9 +0,0 @@
-// Computar o fatorial de um número; 
-
-var nFatorial = 7
-var resultado = nFatorial
-
-for (var contador = 1; contador < nFatorial; contador++){
-	resultado = resultado * contador
-}
-console.log(resultado)
